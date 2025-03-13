e8b274557ca42e4deaae0ec4bab6f0d92f3f6fc0
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2025-03-13 18:27:40 +0100

    test, updates

---
+++ src/test/java/app/resources/controllers/ResourceTest.java
@@ -0,0 +1,70 @@
+package app.resources.controllers;
+
+import app.config.HibernateConfig;
+import app.rest.ApplicationConfig;
+import app.rest.Routes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ResourceTest
+{
+
+    private static final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryForTest();
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeAll
+    static void setupAll()
+    {
+        ApplicationConfig
+            .getInstance()
+            .initiateServer()
+            .setRoute(Routes.getRoutes())
+            .handleExceptions()
+            .startServer(7777);
+        RestAssured.baseURI = "http://localhost:7777/api";
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+
+            // wipe db
+            //em.createQuery("DELETE FROM Room").executeUpdate();
+            //em.createQuery("DELETE FROM Hotel").executeUpdate();
+            em.createNativeQuery("ALTER SEQUENCE room_id_seq RESTART WITH 1");
+            em.createNativeQuery("ALTER SEQUENCE hotel_id_seq RESTART WITH 1");
+
+
+
+            // save to db
+            em.getTransaction().commit();
+
+            em.clear();
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterAll
+    static void tearDown()
+    {
+        if (emf != null && emf.isOpen())
+        {
+            emf.close();
+            System.out.println("EMF is closed....");
+        }
+        ApplicationConfig.getInstance().stopServer();
+    }
+
+
+}
