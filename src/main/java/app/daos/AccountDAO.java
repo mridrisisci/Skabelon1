ed471974ac72c3e3386c9d126942729974295310
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2025-03-14 10:32:36 +0100

    more refactoring

---
+++ src/main/java/app/daos/AccountDAO.java
@@ -0,0 +1,119 @@
+package app.daos;
+
+import app.config.HibernateConfig;
+import app.entities.Role;
+import app.entities.Account;
+import app.exceptions.ValidationException;
+import dk.bugelhartmann.UserDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AccountDAO
+{
+    private GenericDAO genericDAO;
+    private static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+    private static AccountDAO instance;
+    private Logger logger = LoggerFactory.getLogger(AccountDAO.class);
+
+    private AccountDAO(EntityManagerFactory emf)
+    {
+        genericDAO = GenericDAO.getInstance(emf);
+
+    }
+
+    public static AccountDAO getInstance(EntityManagerFactory emf)
+    {
+        if (instance == null)
+        {
+            instance = new AccountDAO(emf);
+        }
+        return instance;
+    }
+
+    public Account create(Account account)
+    {
+        try (var em = emf.createEntityManager())
+        {
+            Set<Role> newRoleSet = new HashSet<>();
+            if (account.getRoles().size() == 0)
+            {
+                Role accountRole = em.find(Role.class, "account");
+                if (accountRole == null)
+                {
+                    accountRole = new Role("account");
+                    em.persist(accountRole);
+                }
+                account.addRole(accountRole);
+            }
+            account.getRoles().forEach(role ->
+            {
+                Role foundRole = em.find(Role.class, role.getName());
+                if (foundRole == null)
+                {
+                    throw new EntityNotFoundException("no role found with this id");
+                } else
+                {
+                    newRoleSet.add(foundRole);
+                }
+            });
+            account.setRoles(newRoleSet);
+            em.getTransaction().begin();
+            em.persist(account);
+            em.getTransaction().commit();
+        } catch (Exception e)
+        {
+            logger.error("Error creating account: " + e.getMessage());
+            return null;
+        }
+        return account;
+    }
+
+    public Role createRole(Role role)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.persist(role);
+            em.getTransaction().commit();
+            return role;
+        }
+    }
+
+    public UserDTO getVerifiedAccount(String username, String password) throws ValidationException
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            Account account = em.find(Account.class, username);
+            if (account == null)
+            {
+                throw new EntityNotFoundException("User not found" + username);
+            }
+            account.getRoles().size();
+            if (!account.verifyPassword(password))
+            {
+                throw new ValidationException("Wrong password");
+            }
+            return new UserDTO(account.getUsername(), account.getRoles().stream()
+                .map(r -> r.getName()).collect(Collectors.toSet()));
+        }
+    }
+
+    public void updateAccount(Account account)
+    {
+
+    }
+
+    public void deleteAccount (Account account)
+    {
+
+    }
+
+
+}
